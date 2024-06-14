670f41667da4608b659b9e9b93cc14fb17bc78df
suprabhosaha <suprabho04@gmail.com>
2024-06-14 18:10:10 +0530

    first-commit

---
+++ SpeechIdentification.py
@@ -0,0 +1,215 @@
+import os
+import wave
+import time
+import pickle
+import pyaudio
+import warnings
+import numpy as np
+from sklearn import preprocessing
+from scipy.io.wavfile import read
+import python_speech_features as mfcc
+from sklearn.mixture import GaussianMixture 
+
+warnings.filterwarnings("ignore")
+
+def calculate_delta(array):
+    rows, cols = array.shape
+    deltas = np.zeros((rows, 20))
+    N = 2
+    for i in range(rows):
+        index = []
+        j = 1
+        while j <= N:
+            if i - j < 0:
+                first = 0
+            else:
+                first = i - j
+            if i + j > rows - 1:
+                second = rows - 1
+            else:
+                second = i + j
+            index.append((second, first))
+            j += 1
+        deltas[i] = (array[index[0][0]] - array[index[0][1]] + (2 * (array[index[1][0]] - array[index[1][1]]))) / 10
+    return deltas
+
+def extract_features(audio, rate):
+    mfcc_feature = mfcc.mfcc(audio, rate, 0.025, 0.01, 20, nfft=1200, appendEnergy=True)    
+    mfcc_feature = preprocessing.scale(mfcc_feature)
+    delta = calculate_delta(mfcc_feature)
+    combined = np.hstack((mfcc_feature, delta)) 
+    return combined
+
+def record_audio_train():
+    Name = input("Please Enter Your Name:")
+    for count in range(5):
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        RATE = 44100
+        CHUNK = 512
+        RECORD_SECONDS = 10
+        audio = pyaudio.PyAudio()
+        print("----------------------record device list---------------------")
+        info = audio.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+        for i in range(0, numdevices):
+            if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+        print("-------------------------------------------------------------")
+        index = int(input())        
+        print("recording via index " + str(index))
+        stream = audio.open(format=FORMAT, channels=CHANNELS,
+                            rate=RATE, input=True, input_device_index=index,
+                            frames_per_buffer=CHUNK)
+        print("recording started")
+        Recordframes = []
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            Recordframes.append(data)
+        print("recording stopped")
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+        OUTPUT_FILENAME = Name + "-sample" + str(count) + ".wav"
+        WAVE_OUTPUT_FILENAME = os.path.join("training_set", OUTPUT_FILENAME)
+        
+        if not os.path.exists("training_set"):
+            os.makedirs("training_set")
+        
+        trainedfilelist = open("training_set_addition.txt", 'a')
+        trainedfilelist.write(OUTPUT_FILENAME + "\n")
+        trainedfilelist.close()
+        
+        waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        waveFile.setnchannels(CHANNELS)
+        waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+        waveFile.setframerate(RATE)
+        waveFile.writeframes(b''.join(Recordframes))
+        waveFile.close()
+
+def record_audio_test():
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    CHUNK = 512
+    RECORD_SECONDS = 10
+    audio = pyaudio.PyAudio()
+    print("----------------------record device list---------------------")
+    info = audio.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+    print("-------------------------------------------------------------")
+    index = int(input())        
+    print("recording via index " + str(index))
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True, input_device_index=index,
+                        frames_per_buffer=CHUNK)
+    print("recording started")
+    Recordframes = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        Recordframes.append(data)
+    print("recording stopped")
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    OUTPUT_FILENAME = "sample.wav"
+    WAVE_OUTPUT_FILENAME = os.path.join("testing_set", OUTPUT_FILENAME)
+    
+    if not os.path.exists("testing_set"):
+        os.makedirs("testing_set")
+    
+    trainedfilelist = open("testing_set_addition.txt", 'a')
+    trainedfilelist.write(OUTPUT_FILENAME + "\n")
+    trainedfilelist.close()
+    
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(Recordframes))
+    waveFile.close()
+
+def train_model():
+    source = "training_set/"   
+    dest = "trained_models/"
+    
+    # Check if the destination directory exists, if not, create it
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+
+    train_file = "training_set_addition.txt"        
+    file_paths = open(train_file, 'r')
+
+    speakers_features = {}
+    
+    for path in file_paths:
+        path = path.strip()
+        speaker_name = path.split("-")[0]
+        
+        if speaker_name not in speakers_features:
+            speakers_features[speaker_name] = np.asarray(())
+
+        sr, audio = read(source + path)
+        vector = extract_features(audio, sr)
+        
+        if speakers_features[speaker_name].size == 0:
+            speakers_features[speaker_name] = vector
+        else:
+            speakers_features[speaker_name] = np.vstack((speakers_features[speaker_name], vector))
+
+    for speaker_name, features in speakers_features.items():
+        if features.shape[0] > 0:
+            gmm = GaussianMixture(n_components=6, max_iter=200, covariance_type='diag', n_init=3)
+            gmm.fit(features)
+            # Save the trained Gaussian model
+            picklefile = speaker_name + ".gmm"
+            with open(os.path.join(dest, picklefile), 'wb') as model_file:
+                pickle.dump(gmm, model_file)
+            print('+ modeling completed for speaker:', picklefile, " with data point = ", features.shape)
+
+def test_model():
+    source = "testing_set/"  
+    modelpath = "trained_models/"
+    test_file = "testing_set_addition.txt"       
+    file_paths = open(test_file, 'r')
+     
+    gmm_files = [os.path.join(modelpath, fname) for fname in os.listdir(modelpath) if fname.endswith('.gmm')]
+    models = [pickle.load(open(fname, 'rb')) for fname in gmm_files]
+    speakers = [os.path.splitext(os.path.basename(fname))[0] for fname in gmm_files]
+
+    threshold = -30  # Example threshold, adjust based on your data and experiments
+     
+    for path in file_paths:
+        path = path.strip()   
+        sr, audio = read(source + path)
+        vector = extract_features(audio, sr)
+         
+        log_likelihood = np.zeros(len(models)) 
+        
+        for i in range(len(models)):
+            gmm = models[i]
+            scores = np.array(gmm.score(vector))
+            log_likelihood[i] = scores.sum()
+         
+        winner = np.argmax(log_likelihood)
+        if log_likelihood[winner] < threshold:
+            print(path, "\tdetected as - Not Identified")
+        else:
+            print(path, "\tdetected as - ", speakers[winner])
+        time.sleep(1.0)
+
+while True:
+    choice = int(input("\n 1.Record audio for training \n 2.Train Model \n 3.Record audio for testing \n 4.Test Model\n"))
+    if choice == 1:
+        record_audio_train()
+    elif choice == 2:
+        train_model()
+    elif choice == 3:
+        record_audio_test()
+    elif choice == 4:
+        test_model()
+    if choice > 4:
+        break
